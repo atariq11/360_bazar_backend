27d23623e35922ec8c877841f2dccfeff10d27be
Aiman <aiman20tariq@gmail.com>
2023-05-16 21:35:11 +0500

    update jwt token code

--- services/jwt.service.js
+++ services/jwt.service.js
@@ -4,7 +4,6 @@ const { line } = require('./app.service');
 const {
     ACCESS_TOKEN_SECRET,
     REFRESH_TOKEN_SECRET,
-    BILLING_TOKEN_SECRET,
     ACCESS_TOKEN_LIFE,
     REFRESH_TOKEN_LIFE
 } = process.env;
@@ -27,22 +26,6 @@ class service {
         const refreshToken = jwt.sign(payload, REFRESH_TOKEN_SECRET, { expiresIn: REFRESH_TOKEN_LIFE });
 
         return { accessToken, refreshToken, ...payload };
-    }
-
-    genBillingToken(payload) {
-        const token = jwt.sign(payload, BILLING_TOKEN_SECRET);
-        return token;
-    }
-
-    validateBillingToken(orderToken) {
-
-        try {
-            return jwt.verify(orderToken, BILLING_TOKEN_SECRET);
-        } catch (error) {
-            const responseMessage = "Invalid orderId token.";
-            log(line().str, responseMessage, error.message);
-            throw new Error(responseMessage);
-        }
     }
 }
 
