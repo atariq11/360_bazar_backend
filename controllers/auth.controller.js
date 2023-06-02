365edf10f3ae394bfaddf2f75db1350c0bc0c543
Aiman <aiman20tariq@gmail.com>
2023-06-02 06:04:32 +0500

    update

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,14 +1,11 @@
 const express = require("express");
-// const bcrypt = require("bcryptjs");
-// const jwt = require("jsonwebtoken");
 const joi = require('joi');
 const { checkAuthentication, checkServiceAccount } = require('../services/auth.service');
 const jwtService = require('../services/jwt.service');
 const emailService = require('../services/email.service');
 const usersModel = require('../models/users.model');
 const rolesModel = require('../models/roles.model');
-// const authenticationMiddleware = require("../middlewares/authentication.middleware");
-// const router = new express.Router();
+
 
 const {
     ACCESS_TOKEN_SECRET,
